2655974aa2540d205c9a8ff0a8e484a0e7b2e62b
Shemar Jones <jones.shemar.sj@gmail.com>
2021-04-17 20:07:39 -0500

    Create Number Line Jumps.py

---
+++ POTW-1/Number Line Jumps.py
@@ -0,0 +1,47 @@
+# Link to the Hackerrank exercise: https://www.hackerrank.com/contests/uwi-comp2211-2021-potw-01/challenges
+
+# -----------------------------------------------
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kangaroo function below.
+
+
+def kangaroo(x1, v1, x2, v2):
+
+    if ((x1 > x2) and (v1 >= v2)) or ((x2 > x1) and (v2 >= v1)):
+        return "NO"
+    if ((x1 == x2) and ((v1 > v2) or (v2 > v1))):
+        return "NO"
+
+    diff = x2 - x1
+
+    if (diff % (v1-v2) == 0):
+        return "YES"
+    else:
+        return "NO"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
